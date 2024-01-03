6a97972a5cdf29667d947984686c92e52ee1d28d
Michael Lueckl <mlueckl@pm.me>
2024-01-03 20:08:13 +0100

    Add console log

--- project/assets.py
+++ project/assets.py
@@ -1,5 +1,6 @@
 import imp
 from pathlib import Path
+from sys import stderr
 from flask import current_app
 
 
@@ -15,3 +16,4 @@ def load_blueprints() -> None:
             bp_name = bp.split(".")[0].split("/")[-1:][0]
             module = imp.load_source(bp_name, str(blueprints_path.joinpath(bp)))
             current_app.register_blueprint(module.__getattribute__(bp_name))
+            print(f" * Blueprint '{bp_name}' loaded", file=stderr)
