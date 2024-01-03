a5c6dc3f0355cea83c801c4ca079302ff5f59713
Michael Lueckl <mlueckl@pm.me>
2024-01-03 20:00:49 +0100

    Minor improvement

--- project/assets.py
+++ project/assets.py
@@ -3,7 +3,7 @@ from pathlib import Path
 from flask import current_app
 
 
-#  Blueprint name must be different from func names
+# Blueprint name must be different from func names
 # TODO: Add option to filter unwanted bp's
 def load_blueprints() -> None:
     blueprints_path = Path().cwd().joinpath("project", "blueprints")
@@ -13,6 +13,5 @@ def load_blueprints() -> None:
 
         if bp.endswith(".py"):
             bp_name = bp.split(".")[0].split("/")[-1:][0]
-            module = imp.load_source(bp_name, str(blueprints_path.joinpath(f"{bp_name}.py")))
-
+            module = imp.load_source(bp_name, str(blueprints_path.joinpath(bp)))
             current_app.register_blueprint(module.__getattribute__(bp_name))
